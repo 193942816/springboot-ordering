03236a767eb310f8c0f663e9445e66aede95f7a4
zhangsan <123456@qq.com>
2022-05-12 13:22:13 +0800

    菜单和订单功能

--- src/main/java/com/example/meal_ordering_system/controller/OrdersController.java
+++ src/main/java/com/example/meal_ordering_system/controller/OrdersController.java
@@ -1,16 +1,32 @@
 package com.example.meal_ordering_system.controller;
 
+import com.example.meal_ordering_system.entity.Orders;
 import com.example.meal_ordering_system.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("orders")
 public class OrdersController {
     @Autowired
     private OrdersService ordersService;
+    //模糊查询：按菜品名称查询，按销售日期查询
+//    @RequestMapping(value = "/order_qiantai",method = RequestMethod.POST)
 
+
+    //查询所有订单
+    @RequestMapping(value = "/ordersgetall",method = RequestMethod.GET)
+    public String allOreders(Orders orders, HttpSession session) {
+        List<Orders> allMenus = ordersService.allmenus(orders);
+        session.setAttribute("ordersearch",allMenus);
+        System.out.println(allMenus);
+        return "/qiantai/index";
+    }
 }
--- src/main/java/com/example/meal_ordering_system/controller/TypesController.java
+++ src/main/java/com/example/meal_ordering_system/controller/TypesController.java
@@ -1,13 +1,50 @@
 package com.example.meal_ordering_system.controller;
 
+import com.example.meal_ordering_system.entity.Types;
 import com.example.meal_ordering_system.service.TypesService;
+import com.example.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("types")
 public class TypesController {
     @Autowired
     private TypesService typesService;
+    //获取全部菜单
+    @GetMapping("queryAll")
+    public String getAll(Integer id, String name, HttpSession session){
+        ResultVo all = typesService.getAll(id, name);
+        System.out.println(all);
+        session.setAttribute("types",all.getData());
+        return "admin/type";
+    }
+    //删除菜单
+    @GetMapping("delete")
+    public String delete( Integer id,String name,HttpSession session){
+        ResultVo vo = typesService.delete(id);
+        this.getAll(id, name, session);
+        return"admin/type";
+    }
+    //添加菜单
+    @PostMapping("insert")
+    public String insert(Types types,HttpSession session){
+        ResultVo add = typesService.add(types);
+        session.setAttribute("items",add);
+        return "admin/type";
+    }
+    //修改菜单
+    @PostMapping("update")
+    public String update(Types types){
+        typesService.update(types);
+        return"admin/type";
+    }
+
+
+
 }
