bd197e16c67da7a97088f4edf0d2c27c312ed2d2
李豪 <123456@qq.com>
2022-05-12 13:13:40 +0800

    管理员登录完成

--- src/main/java/com/example/meal_ordering_system/service/AdminService.java
+++ src/main/java/com/example/meal_ordering_system/service/AdminService.java
@@ -1,4 +1,8 @@
 package com.example.meal_ordering_system.service;
 
+import com.example.meal_ordering_system.vo.ResultVo;
+
 public interface AdminService {
+    ResultVo admin(String name, String pwd);
+
 }
